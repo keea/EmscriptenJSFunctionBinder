c71adffc60bc11a6bebc4fa9461c66b0987caf5e
keea <heea44@naver.com>
2024-02-21 21:22:42 +0900

    feat:#3 Modularize and add test function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
+    std::cout << "hello world main" << std::endl;
     return 0;
+}
+
+EMSCRIPTEN_BINDINGS(Wasm)
+{
+    emscripten::function(
+        "testString", emscripten::optional_override([](const std::string &text)
+                                                    { std::cout << "text : " << text << std::endl; }));
 }
